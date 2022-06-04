96da8592ac78eb8ff7051b88587c6191ecbc910a
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-06-04 22:16:17 +0300

    finish adaptive

---
+++ js/mobile-menu.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".btn__menu--open"),
+    closeMenuBtn: document.querySelector(".btn__menu--close"),
+    menu: document.querySelector(".mob-menu"),
+    body: document.querySelector("body")
+
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle("no-scroll")
+  }
+})();
